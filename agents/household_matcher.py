f1bb550372877f1922f0c66c6d1d235cf68a7033
Aatif123-hub <71110011+Aatif123-hub@users.noreply.github.com>
2024-12-11 13:08:40 -0600

    modified

--- agents/household_matcher.py
+++ agents/household_matcher.py
@@ -14,7 +14,7 @@ class HouseholdMatchAgent:
                         Conduct research on the dataset and provide the Household match result in a tabular format.""" ),
             backstory = dedent("""\
                                As a Household Matching Specialist in the field of entity resolution, 
-                               you must provide the indirect matches in a datset using multiple attributes
+                               you must provide the Household Matches in a datset using multiple attributes
                                Identify records where the full names are the same or have minor spelling variations.
                                Consider abbreviations, nicknames, and common misspellings.
                                Account for case sensitivity and punctuation differences."""),
